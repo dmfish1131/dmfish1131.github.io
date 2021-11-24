3252ceb5d70c0b5921cb7838b86853582c250815
dmfish1131 <daniel.fish@ft.com>
2021-11-24 10:36:54 +0000

    Update experiments.js

--- experiments.js
+++ experiments.js
@@ -3,3 +3,13 @@ const myName = "Daniel Fish";
 const intro = `Hello World! My name is ${myName}`;
 
 console.log(intro);
+
+S
+
+const isSkyBlue = true;
+
+if (isSkyBlue) {
+    console.log("The sky is blue!");
+} else {
+    console.log("The sky is .... not blue?")
+}
